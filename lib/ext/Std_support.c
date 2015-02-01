633c18c0ede7552b82d2e4edd7b99db0ea082ed5
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-02-01 10:54:00 +0900

    fix bug

--- lib/ext/Std_support.c
+++ lib/ext/Std_support.c
@@ -19,46 +19,29 @@ static void obj_dump__(int indent, void *obj)
   switch (OBJ_TYPE(obj)) {
     case OBJTYPE_UNBOXED_ARRAY:
     case OBJTYPE_UNBOXED_VECTOR:
-      if (indent == 0) {
-        for (i = 0; i < OBJ_SIZE(obj) / sizeof(unsigned int); i++)
-          printf("%*s0x%08x\n",
-              0, "", ((unsigned int *)field)[i]);
-        for (i = i * sizeof(unsigned int); i < OBJ_SIZE(obj); i++)
-          printf("%*s0x%02x\n",
-              0, "", ((unsigned char*)field)[i]);
-      } else {
-        printf("%*s%p:%u:%s\n",
-            indent, "", obj, OBJ_SIZE(obj),
-            (OBJ_TYPE(obj) == OBJTYPE_UNBOXED_ARRAY)
-            ? "UNBOXED_ARRAY" : "UNBOXED_VECTOR");
-        for (i = 0; i < OBJ_SIZE(obj) / sizeof(unsigned int); i++)
-          printf("%*s0x%08x\n",
-              indent + 2, "", ((unsigned int *)field)[i]);
-        for (i = i * sizeof(unsigned int); i < OBJ_SIZE(obj); i++)
-          printf("%*s0x%02x\n",
-              indent + 2, "", ((unsigned char*)field)[i]);
-      }
+      printf("%*s%p:%u:%s\n",
+          indent, "", obj, OBJ_SIZE(obj),
+          (OBJ_TYPE(obj) == OBJTYPE_UNBOXED_ARRAY)
+          ? "UNBOXED_ARRAY" : "UNBOXED_VECTOR");
+      for (i = 0; i < OBJ_SIZE(obj) / sizeof(unsigned int); i++)
+        printf("%*s0x%08x\n",
+            indent + 2, "", ((unsigned int *)field)[i]);
+      for (i = i * sizeof(unsigned int); i < OBJ_SIZE(obj); i++)
+        printf("%*s0x%02x\n",
+            indent + 2, "", ((unsigned char*)field)[i]);
       break;
 
     case OBJTYPE_BOXED_ARRAY:
     case OBJTYPE_BOXED_VECTOR:
-      if (indent == 0) {
-        for (i = 0; i < OBJ_SIZE(obj) / sizeof(void*); i++)
-          obj_dump__(0, field[i]);
-        for (i = i * sizeof(void*); i < OBJ_SIZE(obj); i++)
-          printf("%*s0x%02x\n",
-              0, "", ((char*)field)[i]);
-      } else {
-        printf("%*s%p:%u:%s\n",
-            indent, "", obj, OBJ_SIZE(obj),
-            (OBJ_TYPE(obj) == OBJTYPE_BOXED_ARRAY)
-            ? "BOXED_ARRAY" : "BOXED_VECTOR");
-        for (i = 0; i < OBJ_SIZE(obj) / sizeof(void*); i++)
-          obj_dump__(indent + 2, field[i]);
-        for (i = i * sizeof(void*); i < OBJ_SIZE(obj); i++)
-          printf("%*s0x%02x\n",
-              indent + 2, "", ((char*)field)[i]);
-      }
+      printf("%*s%p:%u:%s\n",
+          indent, "", obj, OBJ_SIZE(obj),
+          (OBJ_TYPE(obj) == OBJTYPE_BOXED_ARRAY)
+          ? "BOXED_ARRAY" : "BOXED_VECTOR");
+      for (i = 0; i < OBJ_SIZE(obj) / sizeof(void*); i++)
+        obj_dump__(indent + 2, field[i]);
+      for (i = i * sizeof(void*); i < OBJ_SIZE(obj); i++)
+        printf("%*s0x%02x\n",
+            indent + 2, "", ((char*)field)[i]);
       break;
 
     case OBJTYPE_RECORD:
@@ -87,6 +70,35 @@ static void obj_dump__(int indent, void *obj)
   }
 }
 
+static void first_array_dump(void *obj)
+{
+  unsigned int i;
+  void **field = obj;
+
+  switch (OBJ_TYPE(obj)) {
+    case OBJTYPE_UNBOXED_ARRAY:
+    case OBJTYPE_UNBOXED_VECTOR:
+      for (i = 0; i < OBJ_SIZE(obj) / sizeof(unsigned int); i++)
+        printf("%*s0x%08x\n", 0, "", ((unsigned int *)field)[i]);
+      for (i = i * sizeof(unsigned int); i < OBJ_SIZE(obj); i++)
+        printf("%*s0x%02x\n", 0, "", ((unsigned char*)field)[i]);
+      break;
+
+    case OBJTYPE_BOXED_ARRAY:
+    case OBJTYPE_BOXED_VECTOR:
+      for (i = 0; i < OBJ_SIZE(obj) / sizeof(void*); i++)
+        obj_dump__(0, field[i]);
+      for (i = i * sizeof(void*); i < OBJ_SIZE(obj); i++)
+        printf("%*s0x%02x\n", 0, "", ((char*)field)[i]);
+      break;
+
+    default:
+      printf("%*s%p:%u:unknown type %u",
+          0, "", obj, OBJ_SIZE(obj), OBJ_TYPE(obj));
+      break;
+  }
+}
+
 void c_dump(void* p, size_t size) {
-  obj_dump__(0, p);
+  first_array_dump(p);
 }
